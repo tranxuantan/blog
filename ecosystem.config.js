2bdd5675a9c488a2ae965890224c6e46398294ff
Tan Tran <xuantan0510@gmail.com>
2022-03-03 10:11:25 +0700

    add file ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'blog',
+            port: 3002,
+            exec_mode: 'fork',
+            instances: 1,
+            #script: './node_modules/nuxt/bin/nuxt.js',
+            #args: 'start -c ./nuxt.config.js',
+            cwd: './',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+        }
+    ]
+};
