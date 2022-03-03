f3034958aef830455b8e6aad58bb44530172a19f
tranxuantan <tan.tx@hikotech.io>
2022-03-03 10:45:34 +0700

    add cmd run

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
             port: 3002,
             exec_mode: 'fork',
             instances: 1,
-            #script: './node_modules/nuxt/bin/nuxt.js',
-            #args: 'start -c ./nuxt.config.js',
+            //script: './node_modules/nuxt/bin/nuxt.js',
+            //args: 'start -c ./nuxt.config.js',
             cwd: './',
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
         }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
